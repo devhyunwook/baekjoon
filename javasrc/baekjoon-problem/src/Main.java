4909dd968163e591e347f5ce0ee566cc5946fd10
devhyunwook <nasamjang02@gmail.com>
2019-04-25 14:35:12 +0900

    잘못된 파일 삭제
    ref #master

--- javasrc/baekjoon-problem/src/Main.java
+++ javasrc/baekjoon-problem/src/Main.java
@@ -1,10 +1,11 @@
+import problems2.Problem9012;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        Problem10818 problem = new Problem10818();
-//        problem.scan();
-
+        Problem9012 problem = new Problem9012();
+        problem.scan();
     }
 }
---
+++ javasrc/baekjoon-problem/src/problems2/Problem9012.java
@@ -0,0 +1,14 @@
+package problems2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem9012 {
+    public void scan() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String tmpLine = br.readLine();
+        System.out.println("STEP : " + tmpLine);
+    }
+}
